f52c1025b72f416797e67242f78d96a9278ca3fd
Gabriel Fagundes <gafagundesmesquita@gmail.com>
2025-09-29 09:27:35 -0300

    feat: forçando tipos em ts + unidades absolutas e relativas css

---
+++ TypeScript/Ex1/index.js
@@ -0,0 +1,5 @@
+var firstName = "Gabriel";
+function greeting(name) {
+    console.log("Olá, " + name);
+}
+greeting(firstName);
---
+++ TypeScript/Ex1/index.ts
@@ -0,0 +1,7 @@
+const firstName = "Gabriel";
+
+function greeting(name: string) {
+  console.log("Olá, " + name)
+}
+
+greeting(firstName)
