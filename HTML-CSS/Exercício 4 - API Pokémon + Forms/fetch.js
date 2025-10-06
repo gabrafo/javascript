05473e171732f92a005cfae6caad1b198dfa9641
Gabriel Fagundes <gafagundesmesquita@gmail.com>
2025-10-06 09:00:05 -0300

    feat: simplificando os dados exibidos e tratando caso de input vazio

--- HTML-CSS/Exercício 4 - API Pokémon + Forms/fetch.js
+++ HTML-CSS/Exercício 4 - API Pokémon + Forms/fetch.js
@@ -3,17 +3,19 @@ document.getElementById("pokeForm").addEventListener("submit", async event => {
 
   const formData = new FormData(event.target);
 
-  try {
-    const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${formData.get("pokeName")}`);
-    if (!response.ok) throw new Error("Pokémon não encontrado!");
-    data = await response.json();
-    document.getElementById("result").innerHTML = `
-      <h2>${data.name.toUpperCase()}</h2>
-      <img src="${data.sprites.front_default}" alt="${data.name}">
-      <p><strong>Altura:</strong> ${data.height}</p>
-      <p><strong>Peso:</strong> ${data.weight}</p>
-    `;
-  } catch (err) {
-    document.getElementById("result").textContent = "Erro!" + err.message;
+  let inputValue = formData.get("pokeName");
+
+  if (inputValue) {
+    try {
+      const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${formData.get("pokeName")}`);
+      if (!response.ok) throw new Error("Pokémon não encontrado!");
+      data = await response.json();
+      document.getElementById("result").innerHTML = `
+        <h2>${data.name.toUpperCase()}</h2>
+        <img src="${data.sprites.front_default}" alt="${data.name}">
+      `;
+    } catch (err) {
+      document.getElementById("result").textContent = "Erro!" + err.message;
+    }
   }
 });
